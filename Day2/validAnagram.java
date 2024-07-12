0efff63a7e5c74b5fc09a6eb05a74a31ade1aa23
D Srinivas Karthik <dsrinivaskarthik@gmail.com>
2024-07-12 22:54:50 +0530

    Added comments to ValidAnagram Program

--- Day2/validAnagram.java
+++ Day2/validAnagram.java
@@ -5,17 +5,22 @@ public class validAnagram{
             String s="racecar",t="carrace";
             String s2="jar",t2="jam";
 
+            //create 2 hashmaps to store each character and its occurance for the given two strings
             HashMap<Character,Integer> map = new HashMap<>();
             HashMap<Character,Integer> map2 = new HashMap<>();
-
+            
+            //convert the string to a character array
             for(char x: s.toCharArray()){
-                if(map.containsKey(x)){
-                    map.put(x,map.get(x)+1);
+                
+                if(map.containsKey(x))  //check if the character already exists in the hashmap
+                {
+                    map.put(x,map.get(x)+1);    //increase its previous occurance count by one
                 }
                 else{
-                    map.put(x,1);
+                    map.put(x,1);       //if character is unique, then assign occurance value as 1
                 }
             }
+            //repeat for next string
             for(char x: t.toCharArray()){
                 if(map2.containsKey(x)){
                     map2.put(x,map2.get(x)+1);
@@ -24,6 +29,7 @@ public class validAnagram{
                     map2.put(x,1);
                 }
             }
+            //use .equals() to check if both hashmaps are equal or not and then print boolean value
             System.out.print(map.equals(map2));
 
     }
